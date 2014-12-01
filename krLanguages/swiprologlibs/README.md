7d9ae842296f3ad4b8cab2f9b4bfa8d5f145f31d
Wouter1 <W.Pasman@TUDelft.nl>
2014-12-01 10:33:19 +0100

    #3370 moved swiprolog installer to krLanguages

---
+++ krLanguages/swiprologlibs/README.md
@@ -0,0 +1,7 @@
+SWI Prolog
+==========
+
+Code to make SWI Prolog (binary c++ code) usable from a standard java program.
+It uses the JPL interface.
+
+NOT YET WORKING!
---
+++ krLanguages/swiprologlibs/src/main/java/swiprolog/KRtools.java
@@ -0,0 +1,166 @@
+package swiprolog;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Field;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
+
+import jpl.JPL;
+
+/**
+ * 
+ * After creating KRTools, you may want to call
+ * KRFactory.add(SWIPrologLanguage.getInstance());
+ * 
+ * @author Vincent
+ * @author W.Pasman
+ *
+ */
+public final class KRtools {
+	public final static String JARpath = "";
+	public final static String SWIpath = "";
+
+	static {
+		String JARpathT = "";
+		// final Bundle bundle = Activator.getDefault().getBundle();
+		// final URL found1 = FileLocator.find(bundle, new Path("lib"), null);
+		// final String found2 = FileLocator.resolve(found1).toString()
+		// .replaceAll("\\s", "%20");
+		// final URI found3 = new URI("aap");
+		// JARpathT = new File(found3).getPath();
+		// JARpath = JARpathT;
+		// String SWIpathT = JARpath + File.separator;
+		// final String os = System.getProperty("os.name").toLowerCase(); //
+		// System-dependent
+		// if (os.contains("win")) {
+		// if (System.getProperty("os.version").contains("64")) {
+		// SWIpathT += "win64";
+		// } else {
+		// SWIpathT += "win32";
+		// }
+		// } else if (os.contains("mac")) {
+		// SWIpathT += "mac";
+		// } else {
+		// SWIpathT += "linux";
+		// }
+		// SWIpath = SWIpathT;
+	}
+
+	public static void initializePrologLink() throws Exception {
+		// Dirty system-dependent stuff...
+		addFolderToLibraryPath(SWIpath);
+		if (SWIpath.endsWith("linux")) {
+			System.load(SWIpath + File.separator + "libncurses.so.5");
+			System.load(SWIpath + File.separator + "libreadline.so.6");
+			System.load(SWIpath + File.separator + "libswipl.so.6.0.2");
+			System.load(SWIpath + File.separator + "libjpl.so");
+			System.load(SWIpath + File.separator + "libforeign.so");
+		} else if (SWIpath.endsWith("mac")) {
+			System.load(SWIpath + File.separator + "libncurses.5.4.dylib");
+			System.load(SWIpath + File.separator + "libreadline.6.1.dylib");
+			System.load(SWIpath + File.separator + "libswipl.dylib");
+			System.load(SWIpath + File.separator + "libjpl.dylib");
+			System.load(SWIpath + File.separator + "libforeign.jnilib");
+		} else if (SWIpath.endsWith("win32")) {
+			System.load(SWIpath + File.separator + "pthreadVC.dll");
+			System.load(SWIpath + File.separator + "swipl.dll");
+			System.load(SWIpath + File.separator + "jpl.dll");
+			System.load(SWIpath + File.separator + "foreign.dll");
+		} else if (SWIpath.endsWith("win64")) {
+			System.load(SWIpath + File.separator + "pthreadVC2.dll");
+			System.load(SWIpath + File.separator + "swipl.dll");
+			System.load(SWIpath + File.separator + "jpl.dll");
+			System.load(SWIpath + File.separator + "foreign.dll");
+		}
+		JPL.setDefaultInitArgs(new String[] {
+				// Let JPL know which SWI_HOME_DIR we're using;
+				// this negates the need for a SWI_HOME_DIR environment var
+				"pl", "--home=" + SWIpath, "--quiet", "--nosignals" });
+	}
+
+	private static void addFolderToLibraryPath(final String s) throws Exception {
+		final Field field = ClassLoader.class.getDeclaredField("usr_paths");
+		field.setAccessible(true);
+		final String[] paths = (String[]) field.get(null);
+		for (final String path : paths) {
+			if (s.equalsIgnoreCase(path)) {
+				return;
+			}
+		}
+		final String[] tmp = new String[paths.length + 1];
+		System.arraycopy(paths, 0, tmp, 0, paths.length);
+		tmp[paths.length] = s;
+		field.set(null, tmp);
+		final String path = s + File.pathSeparator
+				+ System.getProperty("java.library.path");
+		System.setProperty("java.library.path", path);
+	}
+
+	/**
+	 * Unzip a given zip file to /tmp.
+	 * 
+	 * @param zipfilename
+	 * @throws URISyntaxException
+	 * @throws IOException
+	 * @throws ZipException
+	 */
+	private static void unzipToTmp(String zipfilename)
+			throws URISyntaxException, ZipException, IOException {
+		URL sourceDirUrl = ClassLoader.getSystemResource("swiprolog/lib/"
+				+ zipfilename);
+		File sourceFile = new File(sourceDirUrl.toURI());
+		ZipFile zipFile = new ZipFile(sourceFile);
+
+		Enumeration<? extends ZipEntry> entries = zipFile.entries();
+		while (entries.hasMoreElements()) {
+			ZipEntry entry = (ZipEntry) entries.nextElement();
+			if (entry.isDirectory()) {
+				// Assume directories are stored parents first then children.
+				System.err.println("Extracting directory: " + entry.getName());
+				// This is not robust, just for demonstration purposes.
+				(new File(entry.getName())).mkdir();
+				continue;
+			}
+			System.err.println("Extracting file: " + entry.getName());
+			copyInputStream(
+					zipFile.getInputStream(entry),
+					new BufferedOutputStream(new FileOutputStream(entry
+							.getName())));
+		}
+		zipFile.close();
+	}
+
+	/**
+	 * Copy all bytes from inputstream to output stream.
+	 * 
+	 * @param in
+	 *            the {@link InputStream}
+	 * @param out
+	 *            the {@link OutputStream}
+	 * @throws IOException
+	 */
+	public static final void copyInputStream(InputStream in, OutputStream out)
+			throws IOException {
+		byte[] buffer = new byte[1024];
+		int len;
+		while ((len = in.read(buffer)) >= 0)
+			out.write(buffer, 0, len);
+		in.close();
+		out.close();
+	}
+
+	public static void main(String[] args) throws ZipException,
+			URISyntaxException, IOException {
+		unzipToTmp("mac.zip");
+	}
+
+}
