dc84d540cecacf3f44b7f79ed9ada0800cae91e2
Wouter1 <W.Pasman@TUDelft.nl>
2015-09-14 16:38:33 +0200

    #3676 unit test showing what seems to be the root cause of #3676

---
+++ krLanguages/swiprolog/src/test/java/swiprolog/language/AssertModule.java
@@ -0,0 +1,57 @@
+/**
+ * Knowledge Representation Tools. Copyright (C) 2014 Koen Hindriks.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package swiprolog.language;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Hashtable;
+
+import jpl.Query;
+
+import org.junit.Test;
+
+import swiprolog.SwiInstaller;
+
+/**
+ * Test if inserting into module works ok. #3676
+ * 
+ * @author W.Pasman 14sep15
+ *
+ */
+public class AssertModule {
+	static {
+		SwiInstaller.init();
+	}
+
+	@Test
+	public void assertIntoModule() {
+		Query insert = new jpl.Query(
+				"assert(:('owner:main:sippingbeer', sippingbeer))");
+		insert.allSolutions();
+
+		Query check = new jpl.Query("'owner:main:sippingbeer':sippingbeer");
+		Hashtable[] result = check.allSolutions();
+		assertEquals(1, result.length);
+
+		Query predicatesq = new jpl.Query(
+				"'owner:main:sippingbeer':(current_predicate(_,Pred), not(predicate_property(Pred, imported_from(_))), not(predicate_property(Pred, built_in)), strip_module(Pred,Module,Head), clause(Head,Body,_))");
+		Hashtable[] preds = predicatesq.allSolutions();
+		assertEquals(1, preds.length);
+	}
+
+}
