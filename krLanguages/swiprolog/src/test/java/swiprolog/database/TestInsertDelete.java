0465b011940dcdfe131dd98b2696b973c50f4c13
Vincent Koeman <vj.koeman@quicknet.nl>
2016-03-01 11:10:07 +0100

    fix the unit test that has just come in from the master

--- krLanguages/swiprolog/src/test/java/swiprolog/database/TestInsertDelete.java
+++ krLanguages/swiprolog/src/test/java/swiprolog/database/TestInsertDelete.java
@@ -6,19 +6,19 @@ import static org.junit.Assert.assertTrue;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-import jpl.Atom;
-import jpl.Compound;
-import krTools.KRInterface;
-import krTools.database.Database;
-import krTools.errors.exceptions.KRDatabaseException;
-import krTools.errors.exceptions.KRQueryFailedException;
-import krTools.language.DatabaseFormula;
-import krTools.language.Substitution;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import jpl.Atom;
+import jpl.Compound;
+import krTools.KRInterface;
+import krTools.database.Database;
+import krTools.exceptions.KRDatabaseException;
+import krTools.exceptions.KRQueryFailedException;
+import krTools.language.DatabaseFormula;
+import krTools.language.Substitution;
+import swiprolog.SwiPrologInterface;
 import swiprolog.language.PrologDBFormula;
 import swiprolog.language.PrologQuery;
 
@@ -35,20 +35,14 @@ public class TestInsertDelete {
 
 	private final Atom p1 = new jpl.Atom("p");
 	private final Atom p2 = new jpl.Atom("p");
-	private final Compound dynamicp = new jpl.Compound("dynamic",
-			new jpl.Term[] { p1 });
+	private final Compound dynamicp = new jpl.Compound("dynamic", new jpl.Term[] { this.p1 });
 
 	@Before
 	public void setUp() throws Exception {
-		this.language = swiprolog.SWIPrologInterface.getInstance();
-
-		this.knowledgebase = this.language
-				.getDatabase(new LinkedHashSet<DatabaseFormula>());
-
-		this.beliefbase = this.language
-				.getDatabase(new LinkedHashSet<DatabaseFormula>());
-
-		beliefbase.query(new PrologQuery(dynamicp, null));
+		this.language = new SwiPrologInterface();
+		this.knowledgebase = this.language.getDatabase("knowledge", new LinkedHashSet<DatabaseFormula>());
+		this.beliefbase = this.language.getDatabase("beliefs", new LinkedHashSet<DatabaseFormula>());
+		this.beliefbase.query(new PrologQuery(this.dynamicp, null));
 	}
 
 	@After
@@ -63,52 +57,48 @@ public class TestInsertDelete {
 
 	/**
 	 * Query p
-	 * 
+	 *
 	 * @return set of solutions for query p.
 	 */
 	Set<Substitution> QueryP() throws KRQueryFailedException {
-		return beliefbase.query(new PrologQuery(p1, null));
+		return this.beliefbase.query(new PrologQuery(this.p1, null));
 	}
 
 	@Test
 	public void testInsert() throws KRDatabaseException, KRQueryFailedException {
-
 		assertTrue(QueryP().isEmpty());
 
-		beliefbase.insert(new PrologDBFormula(p1, null));
+		this.beliefbase.insert(new PrologDBFormula(this.p1, null));
 
 		assertEquals(1, QueryP().size());
 	}
 
 	/**
 	 * You can insert duplicates. But you won't see them as query returns a SET
-	 * 
+	 *
 	 * @throws KRDatabaseException
 	 * @throws KRQueryFailedException
 	 */
 	@Test
-	public void testInsertDuplicate() throws KRDatabaseException,
-			KRQueryFailedException {
-
+	public void testInsertDuplicate() throws KRDatabaseException, KRQueryFailedException {
 		assertTrue(QueryP().isEmpty());
 
-		beliefbase.insert(new PrologDBFormula(p1, null));
-		beliefbase.insert(new PrologDBFormula(p2, null));
+		this.beliefbase.insert(new PrologDBFormula(this.p1, null));
+		this.beliefbase.insert(new PrologDBFormula(this.p2, null));
 
 		assertEquals(1, QueryP().size());
 	}
 
 	/**
 	 * Check that delete deletes ALL duplicates.
-	 * 
+	 *
 	 * @throws KRDatabaseException
 	 * @throws KRQueryFailedException
 	 */
 	@Test
-	public void testDeleteAfterDuplicate() throws KRDatabaseException,
-			KRQueryFailedException {
+	public void testDeleteAfterDuplicate() throws KRDatabaseException, KRQueryFailedException {
 		testInsertDuplicate();
-		beliefbase.delete(new PrologDBFormula(p1, null));
+		this.beliefbase.delete(new PrologDBFormula(this.p1, null));
 		assertTrue(QueryP().isEmpty());
 	}
 
