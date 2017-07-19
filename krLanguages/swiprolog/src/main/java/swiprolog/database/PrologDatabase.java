aa4956cc4ed14939afd1de736cd06e6b68bb72bd
Vincent Koeman <v.j.koeman@tudelft.nl>
2017-07-19 09:18:36 +0200

    fix c-level access exceptions by synchronizing doing SWI queries

--- krLanguages/swiprolog/src/main/java/swiprolog/database/PrologDatabase.java
+++ krLanguages/swiprolog/src/main/java/swiprolog/database/PrologDatabase.java
@@ -40,8 +40,8 @@ import swiprolog.language.PrologSubstitution;
 
 public class PrologDatabase implements Database {
 	/**
-	 * Name of this database; used to name a SWI-Prolog module that implements
-	 * the database.
+	 * Name of this database; used to name a SWI-Prolog module that implements the
+	 * database.
 	 */
 	private final jpl.Atom name;
 	/**
@@ -52,6 +52,10 @@ public class PrologDatabase implements Database {
 	 * A corresponding theory
 	 */
 	private final Theory theory;
+	/**
+	 * Query lock
+	 */
+	private static final Object lock = new Object();
 
 	/**
 	 * @param name
@@ -114,9 +118,9 @@ public class PrologDatabase implements Database {
 	}
 
 	/**
-	 * Performs given query on the database. As databases are implemented as
-	 * modules in SWI Prolog, a query is constructed that contains a reference
-	 * to the corresponding module.
+	 * Performs given query on the database. As databases are implemented as modules
+	 * in SWI Prolog, a query is constructed that contains a reference to the
+	 * corresponding module.
 	 *
 	 * @param pQuery
 	 *            the query to be performed.
@@ -135,10 +139,10 @@ public class PrologDatabase implements Database {
 	}
 
 	/**
-	 * Inserts a set of like {@link #insert(DatabaseFormula)}, but does not add
-	 * them to the theory. This makes sure that they will not show up when the
-	 * set of formulas in this base is requested, and that they cannot be
-	 * modified either (because the theory is always checked for that).
+	 * Inserts a set of like {@link #insert(DatabaseFormula)}, but does not add them
+	 * to the theory. This makes sure that they will not show up when the set of
+	 * formulas in this base is requested, and that they cannot be modified either
+	 * (because the theory is always checked for that).
 	 *
 	 * @param knowledge
 	 *            the set of knowledge that should be imposed on this database.
@@ -153,9 +157,8 @@ public class PrologDatabase implements Database {
 	/**
 	 * <p>
 	 * Inserts formula into SWI prolog database without any checks. You are
-	 * responsible for creating legal SWI prolog query. The formula will be
-	 * prefixed with the label of the database: the SWI prolog query will look
-	 * like <br>
+	 * responsible for creating legal SWI prolog query. The formula will be prefixed
+	 * with the label of the database: the SWI prolog query will look like <br>
 	 * <tt>insert(&lt;database label>:&lt;formula>)</tt>
 	 * </p>
 	 * ASSUMES formula can be argument of assert (fact, rules).
@@ -173,14 +176,14 @@ public class PrologDatabase implements Database {
 	}
 
 	/**
-	 * Inserts positive literals that are part of the update into the database
-	 * and retracts the negative literals. CHECK maybe it is faster to compile
-	 * to a single large JPL call before
+	 * Inserts positive literals that are part of the update into the database and
+	 * retracts the negative literals. CHECK maybe it is faster to compile to a
+	 * single large JPL call before
 	 *
 	 * @param update
-	 *            The basic databaseformula, not(databaseformula) or
-	 *            comma-separated list of dtabase formulas. comma-separated as
-	 *            usual in Prolog: (t1,(t2,(t3,(t4...,(..,tn)))))
+	 *            The basic databaseformula, not(databaseformula) or comma-separated
+	 *            list of dtabase formulas. comma-separated as usual in Prolog:
+	 *            (t1,(t2,(t3,(t4...,(..,tn)))))
 	 * @param database
 	 *            The database into which the update has been inserted.
 	 * @throws KRDatabaseException
@@ -196,8 +199,8 @@ public class PrologDatabase implements Database {
 	}
 
 	/**
-	 * Creates JPL term that wraps given term inside "assert(databaseName:term)"
-	 * for clauses, and just databaseName:term for directives (without the :-).
+	 * Creates JPL term that wraps given term inside "assert(databaseName:term)" for
+	 * clauses, and just databaseName:term for directives (without the :-).
 	 * <p>
 	 * Prefix notation is used below to construct the assert term.
 	 * </p>
@@ -238,16 +241,16 @@ public class PrologDatabase implements Database {
 	/**
 	 * <p>
 	 * Deletes a formula from a SWI Prolog Database. You are responsible for
-	 * creating legal SWI prolog query. The formula will be prefixed with the
-	 * label of the database: the SWI prolog query will look like <br>
+	 * creating legal SWI prolog query. The formula will be prefixed with the label
+	 * of the database: the SWI prolog query will look like <br>
 	 * <tt>retract(&lt;database label>:&lt;formula>)</tt>
 	 * </p>
 	 *
 	 * @param formula
-	 *            is the DatabaseFormula to be retracted from SWI. ASSUMES
-	 *            formula can be argument of retract (fact, rules). CHECK rules
-	 *            need to be converted into string correctly! toString may be
-	 *            insufficient for SWI queries
+	 *            is the DatabaseFormula to be retracted from SWI. ASSUMES formula
+	 *            can be argument of retract (fact, rules). CHECK rules need to be
+	 *            converted into string correctly! toString may be insufficient for
+	 *            SWI queries
 	 * @throws KRDatabaseException
 	 */
 	@Override
@@ -285,15 +288,15 @@ public class PrologDatabase implements Database {
 	 * {@link PrologSubstitution}s.
 	 * </p>
 	 * <p>
-	 * WARNING. this is for internal use in KR implementation only. There is a
-	 * known issue with floats (TRAC #726).
+	 * WARNING. this is for internal use in KR implementation only. There is a known
+	 * issue with floats (TRAC #726).
 	 * </p>
 	 *
 	 * @param query
 	 *            A JPL query.
-	 * @return A set of substitutions, empty set if there are no solutions, and
-	 *         a set with the empty substitution if the query succeeds but does
-	 *         not return any bindings of variables.
+	 * @return A set of substitutions, empty set if there are no solutions, and a
+	 *         set with the empty substitution if the query succeeds but does not
+	 *         return any bindings of variables.
 	 * @throws KRQueryFailedException
 	 */
 	@SuppressWarnings({ "unchecked", "rawtypes" })
@@ -304,7 +307,9 @@ public class PrologDatabase implements Database {
 		// Get all solutions.
 		Hashtable[] solutions;
 		try {
-			solutions = jplQuery.allSolutions();
+			synchronized (lock) {
+				solutions = jplQuery.allSolutions();
+			}
 		} catch (jpl.PrologException e) {
 			throw new PrologError(e);
 		} catch (Throwable e) {
@@ -327,12 +332,11 @@ public class PrologDatabase implements Database {
 	 * Removes all predicates and clauses from the SWI Prolog database.
 	 * </p>
 	 * <p>
-	 * <b>WARNING</b>: This is not implementable fully in SWI prolog. You can
-	 * reset a database to free up some memory, but do not re-use the database.
-	 * It will NOT reset the dynamic declarations. This is an issue but the JPL
-	 * interface to SWI Prolog does not support removing these. Suggested
-	 * workaround: After resetting do not re-use this database but make a new
-	 * one.
+	 * <b>WARNING</b>: This is not implementable fully in SWI prolog. You can reset
+	 * a database to free up some memory, but do not re-use the database. It will
+	 * NOT reset the dynamic declarations. This is an issue but the JPL interface to
+	 * SWI Prolog does not support removing these. Suggested workaround: After
+	 * resetting do not re-use this database but make a new one.
 	 * </p>
 	 * <p>
 	 *
