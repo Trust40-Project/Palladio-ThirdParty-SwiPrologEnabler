b1be6730b451f735a32b7b246b8565a7a5d0c21d
Wouter <w.pasman@tudelft.nl>
2018-07-12 11:43:23 +0200

    SI-93 added junit test at SWI Prolog level showing where the bug comes from.

--- krLanguages/swiprolog/src/test/java/swiprolog/parser/TestKRInterfaceParser.java
+++ krLanguages/swiprolog/src/test/java/swiprolog/parser/TestKRInterfaceParser.java
@@ -1,21 +1,26 @@
 package swiprolog.parser;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import java.io.StringReader;
 
 import org.junit.Test;
 
+import krTools.language.Query;
 import krTools.language.Update;
 import krTools.parser.Parser;
+import krTools.parser.SourceInfo;
 import swiprolog.SwiPrologInterface;
 
 public class TestKRInterfaceParser {
+	private static final SourceInfoObject INFO = new SourceInfoObject("test", 1, 1, 1, 1);
+
 	@Test
 	public void testParseUpdate() throws Exception {
 		new SwiPrologInterface();
 		StringReader reader = new StringReader("on(a,b), on(b,c), on(c,table)");
-		Parser parser = new KRInterfaceParser4(reader, new SourceInfoObject(null, 0, 0, 0, 0));
+		Parser parser = new KRInterfaceParser4(reader, INFO);
 		Update update = parser.parseUpdate();
 
 		assertEquals(",/2", update.getSignature());
@@ -26,10 +31,25 @@ public class TestKRInterfaceParser {
 	public void testParseUpdate_2() throws Exception {
 		new SwiPrologInterface();
 		StringReader reader = new StringReader("zone(ID, Name, X, Y, Neighbours)");
-		Parser parser = new KRInterfaceParser4(reader, new SourceInfoObject(null, 0, 0, 0, 0));
+		Parser parser = new KRInterfaceParser4(reader, INFO);
 		Update update = parser.parseUpdate();
 
 		assertEquals("zone/5", update.getSignature());
 		assertEquals("zone(ID,Name,X,Y,Neighbours)", update.toString());
 	}
+
+	@Test
+	public void parseLessEqualError() throws Exception {
+		// X <= 3 is not correct because "<=" does not exist in prolog.
+		new SwiPrologInterface();
+		StringReader reader = new StringReader("X <= 3");
+		Parser parser = new KRInterfaceParser4(reader, INFO);
+		Query query = parser.parseQuery();
+		for (SourceInfo e : parser.getErrors()) {
+			System.out.println(e);
+			assertFalse(e.toString().contains("basic term 'nothing'"));
+		}
+
+	}
+
 }
